ed00b6e30ff26dd62f554afbf6945b35d0efb366
Grisha Revenko <grisha@revenko.org>
2023-10-21 17:13:55 +0400

    📦 NEW: Add grappling to nearest joint

--- Assets/Scripts/Runtime/_TestScene/PlayerTest.cs
+++ Assets/Scripts/Runtime/_TestScene/PlayerTest.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class PlayerTest : MonoBehaviour
 {
@@ -7,10 +9,14 @@ public class PlayerTest : MonoBehaviour
     [SerializeField] private SpringJoint2D _springJoint2D;
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private Rigidbody2D _rigidbody2D;
-    [SerializeField] private GameObject _jointObject;
-    [SerializeField] private Vector2 _grappleJumpVelocity = new(5f, 8f);
+
+    [Header("Grapple Settings")]
+    [SerializeField, Range(0f, 100f)] private float _grappleRadius = 10f;
+    [SerializeField] private float _grappleJumpVelocityMultiplier = 1.5f;
+    [SerializeField] private LayerMask _jointLayer;
 
     private Transform _thisTransform;
+    private GrapplingJoint _jointObject;
 
     private void Awake()
     {
@@ -29,12 +35,17 @@ public class PlayerTest : MonoBehaviour
         if (_springJoint2D.enabled == true)
             _lineRenderer.SetPosition(1, transform.position);
 
-        _middleObject.SetPosition(_thisTransform.position, _jointObject.transform.position);
+        if (_jointObject != null)
+            _middleObject.SetPosition(_thisTransform.position, _jointObject.transform.position);
     }
 
     private void GrappleJoint()
     {
-        _rigidbody2D.velocity = Vector2.right;
+        bool canGrapple = FindNearestJoint();
+        if (canGrapple == false)
+            return;
+
+        _rigidbody2D.velocity *= Vector2.right * 0.8f;
         _springJoint2D.enabled = true;
         Vector2 jointPosition = _jointObject.transform.position;
         _springJoint2D.connectedAnchor = jointPosition;
@@ -48,9 +59,31 @@ public class PlayerTest : MonoBehaviour
     {
         _springJoint2D.enabled = false;
         if (_rigidbody2D.velocity.x > 0)
-            _rigidbody2D.AddForce(_grappleJumpVelocity, ForceMode2D.Impulse);
+            _rigidbody2D.velocity *= _grappleJumpVelocityMultiplier;
 
         _lineRenderer.enabled = false;
         _focusCamera.StopFocus();
+    }
+
+    private bool FindNearestJoint()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(_thisTransform.position, _grappleRadius, _jointLayer);
+
+        if (colliders == null || colliders.Length == 0)
+            return false;
+
+        Collider2D nearest = colliders
+            .OrderBy(
+                collider => (_thisTransform.position - collider.transform.position).sqrMagnitude)
+            .First();
+
+        _jointObject = nearest.GetComponent<GrapplingJoint>();
+        return _jointObject != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _grappleRadius);
     }
 }
