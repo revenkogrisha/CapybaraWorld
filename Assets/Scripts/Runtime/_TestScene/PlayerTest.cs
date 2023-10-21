86dd9e91ffbf2a3572fcdd98c4ff9dc0326bec95
Grisha Revenko <grisha@revenko.org>
2023-10-21 12:27:35 +0400

    📦 NEW: Add camera follow & zoom

--- Assets/Scripts/Runtime/_TestScene/PlayerTest.cs
+++ Assets/Scripts/Runtime/_TestScene/PlayerTest.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class PlayerTest : MonoBehaviour
 {
+    [SerializeField] private FocusCamera _focusCamera;
     [SerializeField] private SpringJoint2D _springJoint2D;
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private Rigidbody2D _rigidbody2D;
@@ -16,25 +17,33 @@ public class PlayerTest : MonoBehaviour
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-            _rigidbody2D.velocity = Vector2.right;
-            _springJoint2D.enabled = true;
-            Vector2 jointPosition = _jointObject.transform.position;
-            _springJoint2D.connectedAnchor = jointPosition;
-
-            _lineRenderer.enabled = true;
-            _lineRenderer.SetPosition(0, jointPosition);
-        }
+            GrappleJoint();
         else if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            _springJoint2D.enabled = false;
-            if (_rigidbody2D.velocity.x > 0)
-                _rigidbody2D.AddForce(new(10, 10), ForceMode2D.Impulse);
+            ReleaseJoint();
 
-            _lineRenderer.enabled = false;
-        }
-        
         if (_springJoint2D.enabled == true)
             _lineRenderer.SetPosition(1, transform.position);
+    }
+
+    private void GrappleJoint()
+    {
+        _rigidbody2D.velocity = Vector2.right;
+        _springJoint2D.enabled = true;
+        Vector2 jointPosition = _jointObject.transform.position;
+        _springJoint2D.connectedAnchor = jointPosition;
+
+        _lineRenderer.enabled = true;
+        _lineRenderer.SetPosition(0, jointPosition);
+        _focusCamera.StartFocus();
+    }
+
+    private void ReleaseJoint()
+    {
+        _springJoint2D.enabled = false;
+        if (_rigidbody2D.velocity.x > 0)
+            _rigidbody2D.AddForce(new(10, 10), ForceMode2D.Impulse);
+
+        _lineRenderer.enabled = false;
+        _focusCamera.StopFocus();
     }
 }
