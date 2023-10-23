5cb09ab26c9d9cda92e0d5fc1480bd220bedde21
Grisha Revenko <grisha@revenko.org>
2023-10-23 22:39:46 +0400

    📦 NEW: Add platform class

---
+++ Assets/Scripts/Runtime/Level/Platform.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public const float Length = 30f;
+
+    protected void OnDrawGizmos()
+    {
+        float halfWidth = Length * 0.5f;
+
+        Color gizmosColor = Color.red;
+
+        Gizmos.color = gizmosColor;
+
+        Vector3 center = transform.position;
+        Vector3 leftTop = center + Vector3.left * halfWidth + Vector3.up;
+        Vector3 leftBottom = center + Vector3.left * halfWidth + Vector3.down;
+        Vector3 rightTop = center + Vector3.right * halfWidth + Vector3.up;
+        Vector3 rightBottom = center + Vector3.right * halfWidth + Vector3.down;
+
+        Gizmos.DrawLine(leftTop, leftBottom);
+        Gizmos.DrawLine(rightTop, rightBottom);
+        Gizmos.DrawLine(leftTop, rightTop);
+        Gizmos.DrawLine(leftBottom, rightBottom);
+    }
+}
