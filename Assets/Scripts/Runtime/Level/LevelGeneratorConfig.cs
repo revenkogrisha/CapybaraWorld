52b5d08e2288f7a9b6dbf730585b136fd8f5098a
Grisha Revenko <grisha@revenko.org>
2023-10-24 22:23:00 +0400

    📦 NEW: Add Level Generator Config

---
+++ Assets/Scripts/Runtime/Level/LevelGeneratorConfig.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Core.Level
+{
+    [CreateAssetMenu(fileName = "LevelGeneratorConfig", menuName = "Configs/Level Generator Config")]
+    public class LevelGeneratorConfig : ScriptableObject
+    {
+        [field: Header("Platform Generation Settings")]
+        [field: SerializeField] public int PlatformsAmountToGenerate { get; private set; } = 5;
+        [field: SerializeField] public float XStartPoint { get; private set; } = 0f;
+        [field: SerializeField] public float PlatformsY { get; private set; } = -2f;
+
+        [field: Header("Platforms")]
+        [field: SerializeField] public Platform StartPlatform { get; private set; }
+        [field: SerializeField] public Platform[] Platforms { get; private set; }
+    }
+}
