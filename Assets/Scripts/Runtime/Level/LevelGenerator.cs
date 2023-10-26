3b90744da0b68a8d0020d4e455aea70cae6eb177
Grisha Revenko <grisha@revenko.org>
2023-10-26 23:22:12 +0400

    👌 IMPROVE: Return locations to Level Generator

--- Assets/Scripts/Runtime/Level/LevelGenerator.cs
+++ Assets/Scripts/Runtime/Level/LevelGenerator.cs
@@ -7,23 +7,49 @@ using System.Threading;
 using NTC.Pool;
 using Core.Other;
 using Core.Player;
+using UnityTools;
 
 namespace Core.Level
 {
-    public class LevelGenerator : IDisposable
+    public class LevelGenerator
     {
         private const float HeroPositionCheckFrequency = 1.5f;
+        private const int SpecialPlatformSequentialNumber = 4;
+        private const int LocationChangeSequentialNumber = 10;
+        private const float BackgroundLerpDuration = 3.5f;
 
         private readonly LevelGeneratorConfig _config;
-        private readonly Transform _platformsParent;
+
         private readonly Queue<Platform> _platformsOnLevel = new();
-        private readonly Transform _heroTransform;
+        private Camera _camera;
+        private Transform _heroTransform;
         private int _platformNumber = 0;
         private float _lastGeneratedPlatformX = 0f;
+        private int _locationNumber = 0;
+        private Color _defaultBackground;
         private CancellationToken _cancellationToken;
 
         private string PlatformName => $"Platform №{_platformNumber}";
+        private Location CurrentLocation => _config.Locations[_locationNumber];
         private float HeroX => _heroTransform.position.x;
+        
+        private bool IsNowSpecialPlatformTurn 
+        {
+            get
+            {
+                return _platformNumber % SpecialPlatformSequentialNumber == 0
+                && _platformNumber > 0;
+            }
+        }
+
+        private bool IsNowLocationChangeTurn 
+        {
+            get
+            {
+                return _platformNumber % LocationChangeSequentialNumber == 0
+                && _platformNumber > 0;
+            }
+        }
 
         private bool IsLevelMidPointXLessHeroX
         {
@@ -41,48 +67,41 @@ namespace Core.Level
             }
         }
 
-        public LevelGenerator(PlayerTest hero, Transform platformsParent = null)
+        public event Action<float> OnHeroXPositionUpdated;
+        
+        public LevelGenerator(PlayerTest hero)
         {
             _heroTransform = hero.transform;
-            _platformsParent = platformsParent;
+            _lastGeneratedPlatformX = _config.XtartPoint;
+            _camera = Camera.main;
             //_cancellationToken = this.GetCancellationTokenOnDestroy();
 
-            _lastGeneratedPlatformX = _config.XStartPoint;
+            _defaultBackground = _camera.backgroundColor;
         }
 
-
-        public void Dispose()
+        private void Start()
         {
-            // Checkout when does cancel. token is disposed
+            ChangeBackgroundColor();
+
+            CheckPlayerPosition().Forget();
         }
 
-        public void Generate()
-        {
-            GenerateStartPlatform();
-            GenerateDefaultAmount();
-        }
-
-        private void GenerateStartPlatform()
+        public void SpawnStartPlatform()
         {
             GeneratePlatform(_config.StartPlatform);
         }
         
-        private void GenerateDefaultAmount()
+        public void GenerateDefaultAmount()
         {
             for (var i = 0; i < _config.PlatformsAmountToGenerate; i++)
                 GenerateRandomPlatform();
-        }
-
-        public void StartPlayerPositionChecking()
-        {
-            CheckPlayerPosition().Forget();
         }
 
         private async UniTask CheckPlayerPosition()
         {
             while (this != null)
             {
-                await UniTask.WaitUntil(() => _heroTransform != null);
+                OnHeroXPositionUpdated?.Invoke(HeroX);
 
                 if (IsLevelMidPointXLessHeroX == true) 
                 {
@@ -96,8 +115,57 @@ namespace Core.Level
 
         private void GenerateRandomPlatform()
         {
-            Platform randomPlatform = GetRandomPlatform(_config.Platforms);
+            if (IsNowLocationChangeTurn == true)
+            {
+                ChangeLocation();
+                ChangeBackgroundColor();
+            }
+ 
+            Location currentLocation = CurrentLocation;
+            SimplePlatform[] simplePlatforms = currentLocation.SimplePlatforms;
+            SpecialPlatform[] specialPlatforms = currentLocation.SpecialPlatforms;
+
+            Platform randomPlatform;
+            randomPlatform = IsNowSpecialPlatformTurn
+                ? GetRandomPlatform(specialPlatforms)
+                : GetRandomPlatform(simplePlatforms);
+
             GeneratePlatform(randomPlatform);
+        }
+
+        private void ChangeLocation()
+        {
+            _locationNumber++;
+                
+            int locationsLength = _config.Locations.Length;
+            if (_locationNumber >= locationsLength)
+                _locationNumber = _config.Locations.GetRandomIndex();
+        }
+
+        private void ChangeBackgroundColor()
+        {
+            Location currentLocation = CurrentLocation;
+            Color newBackground;
+            if (currentLocation.UseDefaultBackground == true)
+                newBackground = _defaultBackground;
+            else
+                newBackground = currentLocation.BackgroundColor;
+
+            LerpBackground(newBackground, _cancellationToken).Forget();
+        }
+
+        private async UniTask LerpBackground(Color newBackground, CancellationToken token)
+        {
+            float elapsedTime = 0;
+            Color currentBackground = _camera.backgroundColor;
+            while (elapsedTime < BackgroundLerpDuration)
+            {
+                float time = elapsedTime / BackgroundLerpDuration;
+                _camera.backgroundColor = Color.Lerp(currentBackground, newBackground, time);
+
+                elapsedTime += Time.deltaTime;
+                await UniTask.NextFrame(token);
+            }
         }
 
         private T GetRandomPlatform<T>(T[] platformPrefabs)
@@ -110,7 +178,7 @@ namespace Core.Level
         private void GeneratePlatform(Platform platformPrefab)
         {
             Platform platform = SpawnPlatform(platformPrefab);
-            platform.transform.SetParent(_platformsParent);
+            platform.transform.SetParent(_config.PlatformsParent);
             platform.name = PlatformName;
 
             _lastGeneratedPlatformX += Platform.Length;
