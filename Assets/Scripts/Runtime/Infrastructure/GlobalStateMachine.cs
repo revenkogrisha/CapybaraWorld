72effa9ed222a8634623b2260a54d70d5e7f2c0a
Grisha Revenko <grisha@revenko.org>
2023-10-28 20:06:25 +0400

    📦 NEW: Add Global State Machine implementation

---
+++ Assets/Scripts/Runtime/Infrastructure/GlobalStateMachine.cs
@@ -0,0 +1,4 @@
+namespace Core.Infrastructure
+{
+    public class GlobalStateMachine : FiniteStateMachine, IGlobalStateMachine {  }
+}
---
+++ Assets/Scripts/Runtime/Infrastructure/Interfaces/IGlobalStateMachine.cs
@@ -0,0 +1,4 @@
+namespace Core.Infrastructure
+{
+    public interface IGlobalStateMachine : IFiniteStateMachine {  }
+}
