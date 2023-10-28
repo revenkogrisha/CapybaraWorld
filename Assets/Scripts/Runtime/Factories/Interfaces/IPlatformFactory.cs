fa8078ad62f324b34d06cd855693c4e55b80cf26
Grisha Revenko <grisha@revenko.org>
2023-10-28 08:26:09 +0400

    📦 NEW: Add factory interfaces

---
+++ Assets/Scripts/Runtime/Factories/Interfaces/IPlatformFactory.cs
@@ -0,0 +1,13 @@
+using Core.Level;
+using UnityEngine;
+
+namespace Core.Factories
+{
+    public interface IPlatformFactory<TProduct> where TProduct : Platform
+    {
+        public TProduct CreateSimple(Vector2 position);
+
+        public TProduct CreateSpecial(Vector2 position);
+        public TProduct Create(TProduct prefab);
+    }
+}
