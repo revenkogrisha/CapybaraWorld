e70b0eecbfaa889fd9b37279ecc48374319c4af2
Grisha Revenko <grisha@revenko.org>
2023-10-29 23:38:39 +0400

    📦 NEW: Add Bootstrap class

--- Assets/Scripts/Runtime/Infrastructure/Bootstrap.cs
+++ Assets/Scripts/Runtime/Infrastructure/Bootstrap.cs
@@ -1,12 +1,33 @@
 using UnityEngine;
+using Zenject;
 
 namespace Core.Infrastructure
 {
     public class Bootstrap : MonoBehaviour
     {
+        private IGlobalStateMachine _stateMachine;
+
         private void Awake()
         {
+            InitializeGlobalStates(_stateMachine);
             
+            _stateMachine.ChangeState<GenerationState>();
+        }
+
+        [Inject]
+        private void Construct(IGlobalStateMachine stateMachine)
+        {
+            _stateMachine = stateMachine;
+        }
+
+        private void InitializeGlobalStates(IGlobalStateMachine stateMachine)
+        {
+            GenerationState generationState = new();
+
+            stateMachine.AddState<GenerationState>(generationState);
+            stateMachine.AddState<MainMenuState>(generationState);
+            stateMachine.AddState<GameplayState>(generationState);
+            stateMachine.AddState<GameOverState>(generationState);
         }
     }
 }
