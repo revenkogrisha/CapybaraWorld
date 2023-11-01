52d59231ddcbd26d700a6f774741fd850a950483
Grisha Revenko <grisha@revenko.org>
2023-11-01 18:53:17 +0400

    📦 NEW: Add state machine flow

--- Assets/Scripts/Runtime/Infrastructure/States/GameplayState.cs
+++ Assets/Scripts/Runtime/Infrastructure/States/GameplayState.cs
@@ -1,7 +1,36 @@
+using Core.Factories;
+using Core.Level;
+using Core.Player;
+
 namespace Core.Infrastructure
 {
     public class GameplayState : State
     {
+        private readonly ILevelGenerator _levelGenerator;
+        private readonly PlayerFactory _playerFactory;
+        private readonly PlayerDeadlineFactory _playerDeadlineFactory;
+        private readonly IPlayerCamera _playerCamera;
         
+        public GameplayState(
+            ILevelGenerator levelGenerator,
+            PlayerFactory playerFactory,
+            PlayerDeadlineFactory playerDeadlineFactory,
+            IPlayerCamera playerCamera)
+        {
+            _levelGenerator = levelGenerator;
+            _playerFactory = playerFactory;
+            _playerDeadlineFactory = playerDeadlineFactory;
+            _playerCamera = playerCamera;
+        }
+
+        public override void Enter()
+        {
+            PlayerTest hero = _playerFactory.Create();
+            FollowerObject deadline = _playerDeadlineFactory.Create(hero.transform);
+            deadline.BeginFollowing();
+
+            _levelGenerator.ObservePlayer(hero);
+            _playerCamera.Initialize(hero);
+        }
     }
 }
--- Assets/Scripts/Runtime/Infrastructure/States/GenerationState.cs
+++ Assets/Scripts/Runtime/Infrastructure/States/GenerationState.cs
@@ -1,6 +1,4 @@
-using Core.Factories;
 using Core.Level;
-using Core.Player;
 using Zenject;
 
 namespace Core.Infrastructure
@@ -8,37 +6,17 @@ namespace Core.Infrastructure
     public class GenerationState : State
     {
         private readonly ILevelGenerator _levelGenerator;
-        private readonly PlayerFactory _playerFactory;
-        private readonly PlayerDeadlineFactory _playerDeadlineFactory;
-        private readonly IPlayerCamera _playerCamera;
 
         [Inject]
-        public GenerationState(
-            ILevelGenerator levelGenerator,
-            PlayerFactory playerFactory,
-            PlayerDeadlineFactory playerDeadlineFactory,
-            IPlayerCamera camera)
+        public GenerationState(ILevelGenerator levelGenerator)
         {
             _levelGenerator = levelGenerator;
-            _playerFactory = playerFactory;
-            _playerDeadlineFactory = playerDeadlineFactory;
-            _playerCamera = camera;
         }
 
         public override void Enter()
         {
-            PlayerTest hero = _playerFactory.Create();
-            FollowerObject deadline = _playerDeadlineFactory.Create(hero.transform);
-            deadline.BeginFollowing();
-
             _levelGenerator.Generate();
-            _levelGenerator.ObservePlayer(hero);
-
-            _playerCamera.Initialize(hero);
-        }
-
-        public override void Exit()
-        {
+            FiniteStateMachine.ChangeState<MainMenuState>();
         }
     }
 }
