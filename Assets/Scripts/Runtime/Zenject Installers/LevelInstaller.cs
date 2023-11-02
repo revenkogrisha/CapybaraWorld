9a9912d7c050fa389a8f5006882cf14a17b159db
Grisha Revenko <grisha@revenko.org>
2023-11-02 11:53:14 +0400

    🐛 FIX: Add platforms parent

--- Assets/Scripts/Runtime/Zenject Installers/LevelInstaller.cs
+++ Assets/Scripts/Runtime/Zenject Installers/LevelInstaller.cs
@@ -7,6 +7,7 @@ namespace Core.Installers
     public class LevelInstaller : MonoInstaller
     {
         [SerializeField] private LevelGeneratorConfig _levelGeneratorConfig;
+        [SerializeField] private Transform _platformsParent;
 
         public override void InstallBindings()
         {
@@ -15,13 +16,12 @@ namespace Core.Installers
 
         private void BindLevelGenerator()
         {
-            LevelGenerator instance = new(_levelGeneratorConfig);
-            
             Container
                 .Bind<ILevelGenerator>()
                 .To<LevelGenerator>()
-                .FromInstance(instance)
+                .FromNew()
                 .AsSingle()
+                .WithArguments(_levelGeneratorConfig, _platformsParent)
                 .Lazy();
         }
     }
