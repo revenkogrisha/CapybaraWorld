f5ff902216331b24916d3308c25920a3275b6404
Grisha Revenko <grisha@revenko.org>
2023-11-04 16:51:40 +0400

    👌 IMPROVE: Expand IFiniteStateMachine interface

--- Assets/Scripts/Runtime/Infrastructure/Interfaces/IFiniteStateMachine.cs
+++ Assets/Scripts/Runtime/Infrastructure/Interfaces/IFiniteStateMachine.cs
@@ -2,9 +2,22 @@ namespace Core.Infrastructure
 {
     public interface IFiniteStateMachine
     {
-        public void AddState<TState>(State state) where TState : State;
+        public void AddState<TState>(State state) 
+            where TState : State;
 
-        public void ChangeState<TState>() where TState : State;
+        public void ChangeState<TState>() 
+            where TState : State;
+
+        public void ChangeState<TState, TArg>(TArg arg) 
+            where TState : State<TArg>;
+
+        public void ChangeState(State state);
+
+        public TState GetStateWithArg<TState, TArg>(TArg arg) 
+            where TState : State<TArg>;
+
+        public TState GetState<TState>() 
+            where TState : State;
 
         public void DoStateUpdate();
 
