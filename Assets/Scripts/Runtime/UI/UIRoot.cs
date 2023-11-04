0e82af12ae3341d9339ce653a3483dae57f81d3e
Grisha Revenko <grisha@revenko.org>
2023-11-04 09:15:23 +0400

    🐛 FIX: UIRoot.RectTransform is inaccessible

--- Assets/Scripts/Runtime/UI/UIRoot.cs
+++ Assets/Scripts/Runtime/UI/UIRoot.cs
@@ -5,13 +5,8 @@ namespace Core.UI
     [RequireComponent(typeof(RectTransform))]
     public class UIRoot : MonoBehaviour, IUIRoot
     {
-        private RectTransform _rectTransform;
+        [SerializeField] private RectTransform _rectTransform;
 
-        RectTransform IUIRoot.RectTransform => _rectTransform;
-
-        private void Awake()
-        {
-            _rectTransform = GetComponent<RectTransform>();
-        }
+        public RectTransform RectTransform => _rectTransform;
     }
 }
