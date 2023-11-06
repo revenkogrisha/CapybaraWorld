74fb9a48e306b14cdf5182db3405bd2aaf38ce6b
Grisha Revenko <grisha@revenko.org>
2023-11-06 19:17:53 +0400

    👌 IMPROVE: Set target frame rate to 60 in Bootscene

--- Assets/Scripts/Runtime/Infrastructure/Bootscene.cs
+++ Assets/Scripts/Runtime/Infrastructure/Bootscene.cs
@@ -6,13 +6,21 @@ namespace Core.Infrastructure
 {
     public class Bootscene : MonoBehaviour
     {
+        private const int TargetFrameRate = 60;
+
         private LoadingProvider _loadingProvider;
 
         [Inject]
         private void Construct(LoadingProvider loadingProvider) =>
             _loadingProvider = loadingProvider;
 
-        private async void Start() =>
+        private async void Start()
+        {
+            SetupApplication();
             await _loadingProvider.LoadGameWithScreen();
+        }
+
+        private void SetupApplication() =>
+            Application.targetFrameRate = TargetFrameRate;
     }
 }
