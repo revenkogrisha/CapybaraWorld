9eeeea0248a2fa5ba7a0ec84b1339c9c890ecded
Grisha Revenko <grisha@revenko.org>
2023-11-07 17:05:15 +0400

    👌 IMPROVE: Camera Config values

--- Assets/Scripts/Runtime/Player/CameraConfig.cs
+++ Assets/Scripts/Runtime/Player/CameraConfig.cs
@@ -5,8 +5,8 @@ namespace Core.Player
     [CreateAssetMenu(fileName = "Camera Config", menuName = "Configs/Camera Config")]
     public class CameraConfig : ScriptableObject
     {
-        [SerializeField, Min(0f)] private float _regularFov = 8f;
-        [SerializeField, Min(0f)] private float _focusFov = 11f;
+        [SerializeField, Min(0f)] private float _regularFov = 7f;
+        [SerializeField, Min(0f)] private float _focusFov = 10f;
         [SerializeField, Range(0f, 1f)] private float _fovChangeDuration = 0.3f;
 
         public float RegularFov => _regularFov;
