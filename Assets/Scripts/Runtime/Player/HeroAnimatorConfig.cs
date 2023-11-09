cc25761d4294fc0571c57135fda9f22976301e22
Grisha Revenko <grisha@revenko.org>
2023-11-09 11:02:04 +0400

    📦 NEW: Add Hero body rotation

--- Assets/Scripts/Runtime/Player/HeroAnimatorConfig.cs
+++ Assets/Scripts/Runtime/Player/HeroAnimatorConfig.cs
@@ -5,14 +5,23 @@ namespace Core.Player
     [CreateAssetMenu(fileName = "Hero Animator Config", menuName = "Configs/Hero Animator Config")]
     public class HeroAnimatorConfig : ScriptableObject
     {
+        [Header("Body")]
+        [SerializeField] private bool _rotateBody = true;
+        [SerializeField] private float _bodyRotationSpeed = 2f;
+        
         [Header("Arm with Hook")]
         [SerializeField] private bool _rotateArmWithHook = true;
-        [SerializeField] private float _rotationSpeed = 5f;
+        [SerializeField] private float _handRotationSpeed = 5f;
+
+        
+        [Header("Default State")]
         [SerializeField] private Quaternion _defaultRotation = Quaternion.identity;
         [SerializeField] private float _rotateToDefaultDuration = 0.8f;
 
+        public bool RotateBody => _rotateBody;
         public bool RotateArmWithHook => _rotateArmWithHook;
-        public float RotationSpeed => _rotationSpeed;
+        public float HandRotationSpeed => _handRotationSpeed;
+        public float BodyRotationSpeed => _bodyRotationSpeed;
         public Quaternion DefaultRotation => _defaultRotation;
         public float RotateToDefaultDuration => _rotateToDefaultDuration;
     }
