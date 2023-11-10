1e8226431b9f1881e82e159e8c86c85ef2de1114
Grisha Revenko <grisha@revenko.org>
2023-11-10 16:39:27 +0400

    📦 NEW: Add version text

---
+++ Assets/Scripts/Runtime/UI/VersionText.cs
@@ -0,0 +1,19 @@
+using TMPro;
+using UnityEngine;
+
+namespace Core.UI
+{
+    public class VersionText : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _tmp;
+
+        [Space]
+        [SerializeField] private string _prefix = "v. ";
+
+        private void Start()
+        {
+            string version = Application.version;
+            _tmp.text = $"{_prefix}{version}";
+        }
+    }
+}
