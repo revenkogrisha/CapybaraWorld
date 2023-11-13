492a0780ebb2eb5028043ebcaa471ce4aa5554ee
Grisha Revenko <grisha@revenko.org>
2023-11-13 13:27:46 +0400

    👌 IMPROVE: Refactor Gameplay State disposing

--- Assets/Scripts/Runtime/Infrastructure/States/GameplayState.cs
+++ Assets/Scripts/Runtime/Infrastructure/States/GameplayState.cs
@@ -1,22 +1,28 @@
+using System;
+using System.Collections.Generic;
 using Core.Factories;
 using Core.Game;
 using Core.Level;
 using Core.Player;
+using Core.UI;
 using UnityEngine;
 using Inject = Zenject.InjectAttribute;
+using Object = UnityEngine.Object;
 
 namespace Core.Infrastructure
 {
     public class GameplayState : State
     {
+        private readonly List<IDisposable> _disposables = new();
+        private readonly List<GameObject> _destroyables = new();
         private readonly ILevelGenerator _levelGenerator;
         private readonly IFactory<Hero> _playerFactory;
         private readonly IFactory<FollowerObject> _playerDeadlineFactory;
         private readonly IPlayerCamera _playerCamera;
+        private readonly Score _score;
+        private readonly UIProvider _uiProvider;
         private Hero _hero;
-        private FollowerObject _playerDeadline;
         private GameOverHandler _gameOverHandler;
-        private IScoreCounter _scoreCounter;
 
         [Inject]
         public GameplayState(
@@ -24,43 +30,58 @@ namespace Core.Infrastructure
             GameOverHandler gameOverHandler,
             PlayerFactory playerFactory,
             PlayerDeadlineFactory playerDeadlineFactory,
-            IPlayerCamera playerCamera)
+            IPlayerCamera playerCamera,
+            Score score,
+            UIProvider uiProvider)
         {
             _levelGenerator = levelGenerator;
             _gameOverHandler = gameOverHandler;
             _playerFactory = playerFactory;
             _playerDeadlineFactory = playerDeadlineFactory;
             _playerCamera = playerCamera;
+            _score = score;
+            _uiProvider = uiProvider;
+
         }
 
         public override void Enter()
         {
+            _disposables.Clear();
+            _destroyables.Clear();
+
+            _disposables.Add(_playerCamera);
+            _disposables.Add(_gameOverHandler);
+            _disposables.Add(_levelGenerator);
+
             _hero = _playerFactory.Create();
             Transform heroTransform = _hero.transform;
+            _destroyables.Add(_hero.gameObject);
             
-            _playerDeadline = _playerDeadlineFactory.Create();
-            _playerDeadline.BeginFollowing(heroTransform);
+            FollowerObject playerDeadline = _playerDeadlineFactory.Create();
+            playerDeadline.BeginFollowing(heroTransform);
+            _disposables.Add(playerDeadline);
 
-            _gameOverHandler.SubscribeHeroDeath(_hero); // as interface
+            _gameOverHandler.SubscribeHeroDeath(_hero); // remake as interface
 
-            Score score = new();
-            _scoreCounter = new YPositionScoreCounter(heroTransform, score);
+            IScoreCounter scoreCounter = new XPositionScoreCounter(heroTransform, _score);
+            scoreCounter.StartCount();
+            _disposables.Add(scoreCounter);
 
-            _levelGenerator.InitializeCenter(_hero.transform);
+            _levelGenerator.InitializeCenter(heroTransform);
             _playerCamera.Initialize(_hero);
-
-            
         }
 
         public override void Exit()
         {
-            _playerCamera.Dispose();
-            _playerDeadline.Dispose();
-            _gameOverHandler.Dispose();
-            _levelGenerator.Dispose();
-            _scoreCounter.Dispose();
+            foreach (IDisposable disposable in _disposables)
+                disposable.Dispose();
 
-            Object.Destroy(_hero.gameObject);
+            // Made for simplicity - some objects could be just disabled & stored somewhere
+            foreach (GameObject destroyable in _destroyables)
+                Object.Destroy(destroyable);
+
+            _disposables.Clear();
+            _destroyables.Clear();
         }
     }
 }
