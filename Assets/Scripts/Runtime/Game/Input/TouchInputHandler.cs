6cdcf57b58b4d952715a360e0bfe0b3a2846bcca
Grisha Revenko <grisha@revenko.org>
2023-11-15 09:24:31 +0400

    👌 IMPROVE: Refactor Touch Input Handler

--- Assets/Scripts/Runtime/Game/Input/TouchInputHandler.cs
+++ Assets/Scripts/Runtime/Game/Input/TouchInputHandler.cs
@@ -1,39 +1,46 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Core.Game.Input
 {
     public class TouchInputHandler : InputHandler, IDisposable
     {
         private readonly TouchInputCollection _collection;
-        private bool _wasLastTouchOnTheRight;
 
         public TouchInputHandler(TouchInputCollection collection) =>
             _collection = collection;
 
         public override void Initialize()
         {
-            _collection.HoldAction.action.started += _ => HoldStartCommand.Execute();
-            _collection.HoldAction.action.canceled += _ => HoldEndCommand.Execute();
-
-            _collection.HoldAction.action.started += _ => OnPositionHoldActionStarted();
-            _collection.HoldAction.action.canceled += _ => StopCommand.Execute();
-
-            _collection.DashAction.action.performed += _ => DashCommand.Execute();
+            _collection.HoldAction.action.started += OnHoldActionStarted;
+            _collection.HoldAction.action.canceled += OnHoldActionEnded;
+            _collection.DashAction.action.performed += OnDashAction;
         }
 
         public override void Dispose()
         {
-            _collection.HoldAction.action.performed -= _ => HoldStartCommand.Execute();
-            _collection.HoldAction.action.canceled -= _ => HoldEndCommand.Execute();
-
-            _collection.HoldAction.action.started -= _ => OnPositionHoldActionStarted();
-            _collection.HoldAction.action.canceled -= _ => StopCommand.Execute();
-
-            _collection.DashAction.action.performed -= _ => DashCommand.Execute();
+            _collection.HoldAction.action.started -= OnHoldActionStarted;
+            _collection.HoldAction.action.canceled -= OnHoldActionEnded;
+            _collection.DashAction.action.performed -= OnDashAction;
         }
 
-        private void OnPositionHoldActionStarted()
+        private void OnHoldActionStarted(InputAction.CallbackContext context)
+        {
+            HoldStartCommand.Execute();
+            ExecuteMove();
+        }
+
+        private void OnHoldActionEnded(InputAction.CallbackContext context)
+        {
+            HoldEndCommand.Execute();
+            StopCommand.Execute();
+        }
+
+        private void OnDashAction(InputAction.CallbackContext context) => 
+            DashCommand.Execute();
+
+        private void ExecuteMove()
         {
             Vector2 touchPosition = _collection.PositionAction.action.ReadValue<Vector2>();
             bool touchIsOnRight = touchPosition.x > Screen.width * 0.5f;
