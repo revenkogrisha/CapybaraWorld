610edd06bf00740546406fd0e2e52e7128755021
Grisha Revenko <grisha@revenko.org>
2023-11-18 17:17:36 +0400

    📦 NEW: Add Enemy base

---
+++ Assets/Scripts/Runtime/Level/Environment/Enemies/States/EnemyChaseState.cs
@@ -0,0 +1,73 @@
+using System.Threading;
+using Core.Infrastructure;
+using Cysharp.Threading.Tasks;
+using UniRx;
+using UniRx.Triggers;
+using UnityEngine;
+
+namespace Core.Level
+{
+	public class EnemyChaseState : State<Vector2>
+	{
+		private readonly CompositeDisposable _disposable = new();
+		private readonly Transform _thisTransform;
+		private readonly Enemy _enemy;
+		private Vector3
+		 _targetPosition;
+		private Vector2 _directionToTarget;
+
+		public EnemyChaseState(Enemy enemy)
+		{
+			_enemy = enemy;
+			_thisTransform = _enemy.transform;
+		}
+
+		public override void SetArg(Vector2 arg)
+		{
+			_targetPosition = arg;
+			_directionToTarget = GetDirectionToTarget();
+		}
+
+		public override void Enter()
+		{
+			ChaseTarget().Forget();
+		}
+
+		public override void Exit()
+		{
+			_disposable.Clear();
+		}
+
+		private async UniTaskVoid ChaseTarget()
+		{
+			CancellationToken token = _enemy.destroyCancellationToken;
+			bool canceled = false;
+			
+			float elapsedTime = 0f;
+			while (canceled == false)
+			{
+				float distance = Vector2.Distance(_thisTransform.position, _targetPosition);
+					
+				if (distance <= 0.5f)
+				{
+					FiniteStateMachine.ChangeState<EnemyIdleState>();
+					break;
+				}
+				
+				Rigidbody2D rigidbody2D = _enemy.Rigidbody2D;
+				Vector2 velocity = rigidbody2D.velocity;
+				velocity.x = _directionToTarget.x * _enemy.Config.Speed;
+				
+				rigidbody2D.velocity = velocity;
+				
+				elapsedTime += Time.deltaTime;
+				canceled = await UniTask
+					.NextFrame(token)
+					.SuppressCancellationThrow();
+			}
+		}
+
+		private Vector2 GetDirectionToTarget() =>
+			(_targetPosition - _thisTransform.position).normalized;
+	}
+}
---
+++ Assets/Scripts/Runtime/Level/Environment/Enemies/States/EnemyIdleState.cs
@@ -0,0 +1,81 @@
+using System.Threading;
+using Core.Common;
+using Core.Infrastructure;
+using Core.Other;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Core.Level
+{
+    public class EnemyIdleState : State
+	{
+		private readonly Transform _thisTransform;
+		private readonly Enemy _enemy;
+		private LookingDirection _direction;
+		private CancellationTokenSource _cancellationSource;
+
+		public EnemyIdleState(Enemy enemy)
+		{
+			_enemy = enemy;
+			_thisTransform = enemy.transform;
+		}
+
+		public override void Enter()
+		{
+			StopMoving();
+			StartLookingForTarget().Forget();
+		}
+
+		public override void Exit()
+		{
+			_cancellationSource?.Cancel();
+			_cancellationSource?.Dispose();
+			_cancellationSource = null;
+		}
+		
+		private void StopMoving()
+		{
+			Vector2 velocity = Vector2.zero;
+			_enemy.Rigidbody2D.velocity = velocity;
+		}
+
+		private async UniTaskVoid StartLookingForTarget()
+		{
+			_cancellationSource = new();
+			CancellationToken token = _cancellationSource.Token;
+
+			bool canceled = false;
+			while (canceled == false)
+			{
+				Vector2 origin = _thisTransform.position;
+				Vector2 direction = Vector2.left * (float)_direction;
+				float spotRadius = _enemy.Config.SpotRadius;
+				LayerMask targetLayer = _enemy.Config.TargetLayer;
+
+				RaycastHit2D hit = Physics2D.Raycast(origin, direction, spotRadius, targetLayer);
+				if (hit == true)
+				{
+					Vector2 targetPosition = hit.transform.position;
+					FiniteStateMachine.ChangeState<EnemyChaseState, Vector2>(targetPosition);
+					break;
+				}
+
+				ChangeDirection();
+
+				canceled = await MyUniTask
+					.Delay(_enemy.Config.SpotInterval, token)
+					.SuppressCancellationThrow();
+			}
+
+			_cancellationSource?.Dispose();
+			_cancellationSource = null;
+		}
+
+		private void ChangeDirection()
+		{
+			_direction = _direction == LookingDirection.Left
+				? LookingDirection.Right
+				: LookingDirection.Left;
+		}
+	}
+}
