ff81bd4ecebd7631bd08f491d9f0ed249117555d
Grisha Revenko <grisha@revenko.org>
2023-11-19 17:34:58 +0400

    👌 IMPROVE: Add Y parallax effect option

--- Assets/Scripts/Runtime/Level/Parallax.cs
+++ Assets/Scripts/Runtime/Level/Parallax.cs
@@ -9,12 +9,15 @@ namespace Core.Level
     {
         [SerializeField] private Transform _camera;
         [SerializeField] private SpriteRenderer _spriteRenderer;
-        [SerializeField, Range(0f, 1f)] private float _effect;
+        [SerializeField] private bool _applyX = true;
+        [SerializeField, Range(0f, 1f)] private float _effectX;
+        private bool _applyY = false;
+        [SerializeField, Range(0f, 1f)] private float _effectY;
 
         private readonly CompositeDisposable _disposable = new();
         private float _length;
         private Transform _thisTransform;
-        private float _startX;
+        private Vector2 _startPosition;
         private float _temp;
 
         #region MonoBehaviour
@@ -22,7 +25,7 @@ namespace Core.Level
         private void Awake()
         {
             _thisTransform = transform;
-            _startX = _thisTransform.position.x;
+            _startPosition = _thisTransform.position;
             _length = _spriteRenderer.bounds.size.x;
         }
 
@@ -42,35 +45,45 @@ namespace Core.Level
                 .AddTo(_disposable);
 
             fixedUpdate
-                .Where(_ => _temp > _startX + _length)
+                .Where(_ => _temp > _startPosition.x + _length)
                 .Subscribe(_ => MoveRight())
                 .AddTo(_disposable);
 
             fixedUpdate
-                .Where(_ => _temp < _startX - _length)
+                .Where(_ => _temp < _startPosition.x - _length)
                 .Subscribe(_ => MoveLeft())
                 .AddTo(_disposable);
         }
 
         private void Perform()
         {
-            float distance = _camera.position.x * _effect;
-            _temp = _camera.position.x * (1 - _effect);
+            _temp = _camera.position.x * (1 - _effectX);
 
-            Vector2 parallaxed = GetParallaxedPosition(distance);
+            Vector2 parallaxed = GetParallaxedPosition();
             _thisTransform.position = parallaxed;
         }
 
         private void MoveRight() =>
-            _startX += _length;
+            _startPosition.x += _length;
 
         private void MoveLeft() =>
-            _startX -= _length;
+            _startPosition.x -= _length;
 
-        private Vector2 GetParallaxedPosition(float distance)
+        private Vector2 GetParallaxedPosition()
         {
             Vector2 parallaxed = _thisTransform.position;
-            parallaxed.x = _startX + distance;
+            if (_applyX == true)
+            {
+                float distanceX = _camera.position.x * _effectX;
+                parallaxed.x = _startPosition.x + distanceX;
+            }
+
+            if (_applyY == true)
+            {
+                float distanceY = _camera.position.y * _effectY;
+                parallaxed.y = _startPosition.y + distanceY;
+            }
+
             return parallaxed;
         }
     }
