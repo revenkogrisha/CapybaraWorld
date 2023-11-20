23b0f183e6d45098bad61a93f87528fa1a749dac
Grisha Revenko <grisha@revenko.org>
2023-11-20 09:30:30 +0400

    📦 NEW: Add Grappling Joint on grappled animation

--- Assets/Scripts/Runtime/Player/GrapplingJoint.cs
+++ Assets/Scripts/Runtime/Player/GrapplingJoint.cs
@@ -1,6 +1,30 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace Core.Level
 {
-    public class GrapplingJoint : MonoBehaviour {  }
+    public class GrapplingJoint : MonoBehaviour
+    {
+        private const float GrappledScaleMultiplier = 0.8f;
+        private const float ScaleDuration = 0.4f;
+
+        [SerializeField] private ParticleSystem _particles;
+        
+        public void OnGrappled()
+        {
+            if (_particles != null)
+                _particles.Stop();
+
+            TweenScale();
+        }
+
+        private void TweenScale()
+        {
+            const int yoyoLoops = 2;
+
+            Vector3 scale = transform.localScale;
+            scale *= GrappledScaleMultiplier;
+            transform.DOScale(scale, ScaleDuration).SetLoops(yoyoLoops, LoopType.Yoyo);
+        }
+    }
 }
