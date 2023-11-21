ad9f434f0bd5b28f9c2617e9d4cfdd2ec50dfd68
Grisha Revenko <grisha@revenko.org>
2023-11-21 19:17:19 +0400

    🐛 FIX: Add Collisions Extensions

--- Assets/Scripts/Runtime/Extensions/GameObjectExtensions.cs
+++ Assets/Scripts/Runtime/Extensions/GameObjectExtensions.cs
@@ -1,24 +1,10 @@
-﻿using System.Collections.Generic;
-using NTC.Pool;
+﻿using NTC.Pool;
 using UnityEngine;
 
-namespace UnityTools
+namespace Core.Other
 {
     public static class GameObjectExtensions
     {
-        /// <summary>
-        /// Returns array with all materials attached to GameObject
-        /// </summary
-        public static Material[] GetAllMaterials(this GameObject gameObject)
-        {
-            Renderer[] renderers = gameObject.GetComponentsInChildren<Renderer>();
-            var materials = new List<Material>();
-            foreach (var renderer in renderers)
-                materials.Add(renderer.material);
-
-            return materials.ToArray();
-        }
-
         public static bool HasComponent<T>(this GameObject container) 
             where T : Object =>
             container.GetComponent<T>();
