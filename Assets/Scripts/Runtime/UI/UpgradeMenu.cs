89638275c9f91ed3c57197292ba96d6ff106e124
Grisha Revenko <grisha@revenko.org>
2023-12-12 22:19:27 +0400

    NEW: Add player upgrade UI

--- Assets/Scripts/Runtime/UI/UpgradeMenu.cs
+++ Assets/Scripts/Runtime/UI/UpgradeMenu.cs
@@ -1,24 +1,85 @@
+using System;
+using Core.Player;
+using Core.Saving;
 using Cysharp.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityTools.Buttons;
+using Zenject;
 
 namespace Core.UI
 {
     public class UpgradeMenu : AnimatedUI
     {
-        [Space]
+        private const string CostFormat = "{0}";
+        private const string HeroLevelFormat = "Hero Level: {0}";
+        
+        [Header("Upgrade UI")]
+        [SerializeField] private ResourcePanel _resourcePanel;
+        
+        [SerializeField] private UIButton _heroUpgradeButton;
         [SerializeField] private UIButton _backButton;
+
+        [Space] 
+        [SerializeField] private TMP_Text _costTMP;
+        [SerializeField] private TMP_Text _heroLevelTMP;
         
         private MainMenuRoot _root;
+        private PlayerUpgrade _playerUpgrade;
+        private ISaveService _saveService;
 
+        #region MonoBehaviour
+
+        private void OnEnable()
+        {
+            _heroUpgradeButton.OnClicked += UpgradeHero;
+            _backButton.OnClicked += ToMainMenu;
+            
+            UpdateDisplayedData();
+            ValidateButton();
+        }
+
+        private void OnDisable()
+        {
+            _heroUpgradeButton.OnClicked -= UpgradeHero;
+            _backButton.OnClicked -= ToMainMenu;
+        }
+
+        private void Start() => 
+            UpdateDisplayedData();
+
+        #endregion
+
+        [Inject]
+        private void Construct(PlayerUpgrade playerUpgrade, ISaveService saveService)
+        {
+            _playerUpgrade = playerUpgrade;
+            _saveService = saveService;
+        }
+        
         public void InitializeRoot(MainMenuRoot root) =>
             _root = root;
 
-        private void OnEnable() => 
-            _backButton.OnClicked += ToMainMenu;
+        private void ValidateButton() => 
+            _heroUpgradeButton.OriginalButton.interactable = _playerUpgrade.CanUpgradeHero;
 
-        private void OnDisable() => 
-            _backButton.OnClicked -= ToMainMenu;
+        private void UpdateDisplayedData()
+        {
+            _resourcePanel.DisplayResources();
+            
+            _costTMP.SetText(string.Format(CostFormat, _playerUpgrade.Cost));
+            _heroLevelTMP.SetText(string.Format(HeroLevelFormat, _playerUpgrade.HeroLevel));
+        }
+
+        private void UpgradeHero()
+        {
+            _playerUpgrade.UpgradeHero();
+            _saveService.Save();
+            
+            UpdateDisplayedData();
+            
+            ValidateButton();
+        }
 
         private void ToMainMenu()
         {
