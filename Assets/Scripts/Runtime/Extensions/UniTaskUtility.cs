9d0bb1f4e54c3ea269c5f9a4c1a211ac18deb973
Grisha Revenko <grisha@revenko.org>
2023-12-19 23:38:09 +0400

    IMPROVE: Remove unnecessary UniTaskUtility methods

--- Assets/Scripts/Runtime/Extensions/UniTaskUtility.cs
+++ Assets/Scripts/Runtime/Extensions/UniTaskUtility.cs
@@ -6,18 +6,6 @@ namespace Core.Other
 {
     public static class UniTaskUtility
     {
-        public static UniTask Delay(int seconds) => 
-            Delay((float)seconds);
-
-        public static UniTask Delay(int seconds, CancellationToken token) => 
-            Delay((float)seconds, token);
-
-        public static UniTask Delay(float seconds)
-        {
-            TimeSpan delay = TimeSpan.FromSeconds(seconds);
-            return UniTask.Delay(delay);
-        }
-
         public static UniTask Delay(float seconds, CancellationToken token)
         {
             TimeSpan delay = TimeSpan.FromSeconds(seconds);
