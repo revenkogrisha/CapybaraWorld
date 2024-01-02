b28c33a67ebb40e018254a6c5d274dc5645db978
Grisha Revenko <grisha@revenko.org>
2024-01-02 17:04:16 +0400

    📦NEW: Add custom build menu items helpers

--- Assets/Scripts/Editor/BuildHelper.cs
+++ Assets/Scripts/Editor/BuildHelper.cs
@@ -1,4 +1,9 @@
+using System;
+using System.IO;
+using System.Linq;
+using Core.Editor.Debugger;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 
 namespace Core.Editor
 {
@@ -6,6 +11,12 @@ namespace Core.Editor
     {
         private const string DevDirective = "REVENKO_DEVELOP";
         private const string ReleaseDirective = "REVENKO_RELEASE";
+        
+        private const string FileNameFormat = "rel{0}.apk";
+        private const string DevFileNameFormat = "dev{0}.apk";
+        private const string ReleaseFileNameFormat = "CapybaraWorld{0}.aab";
+        
+        private const string RelativeOutputFolder = "Documents/GameProjects/Capybara World/Builds";
 
         [MenuItem("Tools/CapybaraWorld/Build/Set DEFAULT State", false, 50)]
         public static void SetDefaultState()
@@ -27,6 +38,56 @@ namespace Core.Editor
             AddScriptingDefineSymbol(ReleaseDirective);
             RemoveScriptingDefineSymbol(DevDirective);
         }
+
+#if UNITY_ANDROID
+        [MenuItem("Tools/CapybaraWorld/Build/Build DEV for Android", false, 70)]
+#else
+        [MenuItem("Tools/CapybaraWorld/Build/Build DEV for <unknown> platform", false, 70)]
+#endif
+        public static void BuildDev()
+        {
+            SetDevState();
+
+            EditorUserBuildSettings.buildAppBundle = false;
+            EditorUserBuildSettings.development = true;
+            
+            BuildOptions buildOptions = BuildOptions.StrictMode | BuildOptions.Development | BuildOptions.CompressWithLz4;
+        
+            buildOptions = EditorUserBuildSettings.allowDebugging == true
+                ? buildOptions | BuildOptions.AllowDebugging
+                : buildOptions;
+            
+            BuildCurrentPlatform(buildOptions, DevFileNameFormat);
+        }
+        
+#if UNITY_ANDROID
+        [MenuItem("Tools/CapybaraWorld/Build/Build RELEASE for Android", false, 71)]
+#else
+        [MenuItem("Tools/CapybaraWorld/Build/Build RELEASE for <unknown> platform", false, 71)]
+#endif
+        public static void BuildRelease()
+        {
+            SetReleaseState();
+            
+            EditorUserBuildSettings.development = false;
+            EditorUserBuildSettings.allowDebugging = false;
+            
+            BuildOptions buildOptions = BuildOptions.StrictMode | BuildOptions.CompressWithLz4HC;
+            
+            buildOptions = EditorUserBuildSettings.exportAsGoogleAndroidProject == true
+                ? buildOptions | BuildOptions.AcceptExternalModificationsToPlayer
+                : buildOptions;
+
+            string fileNameFormat = EditorUserBuildSettings.buildAppBundle == true
+                ? ReleaseFileNameFormat
+                : FileNameFormat;
+            
+            BuildCurrentPlatform(buildOptions, fileNameFormat);
+        }
+
+        [MenuItem("Tools/CapybaraWorld/Build/Open builds folder", false, 72)]
+        public static void OpenBuildsFolder() =>
+            EditorUtility.RevealInFinder(GetAbsoluteOutputFolder());
         
         private static void AddScriptingDefineSymbol(string defineSymbol)
         {
@@ -45,6 +106,64 @@ namespace Core.Editor
             {
                 defines = defines.Replace(defineSymbol, "");
                 PlayerSettings.SetScriptingDefineSymbolsForGroup(EditorUserBuildSettings.selectedBuildTargetGroup, defines);
+            }
+        }
+
+        private static void BuildCurrentPlatform(BuildOptions buildOptions = BuildOptions.StrictMode, string fileNameFormat = FileNameFormat)
+        {
+            BuildTarget buildTarget = EditorUserBuildSettings.activeBuildTarget;
+            
+            string absoluteOutputFolder = GetAbsoluteOutputFolder();
+            string defaultFileName = string.Format(fileNameFormat, PlayerSettings.bundleVersion);
+            
+            string chosenOutput = EditorUtility.SaveFilePanel("Save Build", absoluteOutputFolder, defaultFileName, "");
+            if (string.IsNullOrEmpty(chosenOutput) == true)
+                return;
+            
+            BuildPlayerOptions buildPlayerOptions = new()
+            {
+                scenes = GetScenes(),
+                locationPathName = chosenOutput,
+                target = buildTarget,
+                options = buildOptions
+            };
+            
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+            if (report.summary.result == BuildResult.Succeeded)
+                EditorUtility.RevealInFinder(chosenOutput);
+            
+            LogBuild(report, buildPlayerOptions);
+        }
+
+        private static string GetAbsoluteOutputFolder()
+        {
+            string homeDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string absoluteOutputFolder = Path.Combine(homeDirectory, RelativeOutputFolder);
+
+            if (Directory.Exists(absoluteOutputFolder) == false)
+                Directory.CreateDirectory(absoluteOutputFolder);
+            
+            return absoluteOutputFolder;
+        }
+
+        private static string[] GetScenes()
+        {
+            return EditorBuildSettings.scenes
+                .Where(scene => scene.enabled)
+                .Select(scene => scene.path)
+                .ToArray();
+        }
+
+        private static void LogBuild(BuildReport report, BuildPlayerOptions buildPlayerOptions)
+        {
+            if (report.summary.result == BuildResult.Succeeded)
+            {
+                RDebug.Info($"Build complete: {buildPlayerOptions.locationPathName}");
+            }
+            else
+            {
+                RDebug.Error($"Build failed: {report.summary.result.ToString()}");
             }
         }
     }
