757d7c957774ff3b865465f01ac9f1c6d192c25f
Grisha Revenko <grisha@revenko.org>
2024-01-09 11:04:36 +0400

    👌 IMPROVE: Decrease win score

--- Assets/Scripts/Runtime/Game/PlaythroughHandler.cs
+++ Assets/Scripts/Runtime/Game/PlaythroughHandler.cs
@@ -10,7 +10,7 @@ namespace Core.Game
 {
     public class PlaythroughHandler : IPlaythroughProgressHandler, IGameEventsHandler, IDisposable
     {
-        private const int WinScore = 115;
+        private const int WinScore = 110;
         
         private readonly ILocationsHandler _locationsHandler;
         private readonly GameNavigation _navigation;
