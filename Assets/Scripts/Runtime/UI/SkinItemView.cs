16504c9fe5c962ade51d6d86697810e9d1864061
Grisha Revenko <grisha@revenko.org>
2024-01-11 23:02:12 +0400

    📦 NEW: Add Skin Item View tweening

--- Assets/Scripts/Runtime/UI/SkinItemView.cs
+++ Assets/Scripts/Runtime/UI/SkinItemView.cs
@@ -1,5 +1,6 @@
 using Core.Editor.Debugger;
 using Core.Player;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -7,16 +8,38 @@ using UnityEngine.UI;
 
 namespace Core.UI
 {
-    public class SkinItemView : MonoBehaviour, IPointerClickHandler
+    public class SkinItemView : MonoBehaviour, 
+        IPointerDownHandler, IPointerUpHandler, IPointerClickHandler
     {
-        private SkinsPanel _panel;
+        private const float ClickScale = 0.85f;
+        private const float ClickTweenDuration = 0.08f;
         
-        [Space]
         [SerializeField] private GameObject _selectedPanel;
         [SerializeField] private Image _image;
         [SerializeField] private TMP_Text _nameTMP;
 
+        private SkinsPanel _panel;
+        private bool _isTweeningScale = false;
+
         public SkinName Name { get; private set; }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (_isTweeningScale == false)
+            {
+                _isTweeningScale = true;
+                transform.DOScale(ClickScale, ClickTweenDuration);
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            if (_isTweeningScale == true)
+            {
+                transform.DOScale(1f, ClickTweenDuration);
+                _isTweeningScale = false;
+            }
+        }
 
         public void OnPointerClick(PointerEventData eventData)
         {
@@ -37,6 +60,8 @@ namespace Core.UI
 
             _image.sprite = menuItem;
             _nameTMP.SetText(Name.ToString());
+
+            OnPointerUp(null);
         }
 
         public void SetSelected(bool value) => 
