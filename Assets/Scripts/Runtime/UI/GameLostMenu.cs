68d469745434027439c2755a16d23fee46de82ec
Grisha Revenko <grisha@revenko.org>
2024-01-12 11:44:17 +0400

    📦 NEW: Add custom progress bar hero icon on Lost Menu

--- Assets/Scripts/Runtime/UI/GameLostMenu.cs
+++ Assets/Scripts/Runtime/UI/GameLostMenu.cs
@@ -1,4 +1,5 @@
 using Core.Game;
+using Core.Player;
 using UnityEngine;
 using UnityEngine.UI;
 using Zenject;
@@ -7,17 +8,25 @@ namespace Core.UI
 {
     public class GameLostMenu : FinishMenu
     {
-        [Space]
+        [Header("Progress Bar")]
         [SerializeField] private Slider _progressBar;
+        [SerializeField] private Image _heroImage;
 
         protected IPlaythroughProgressHandler _playthrough;
+        private HeroSkins _heroSkins;
 
-        private void Start() => 
+        private void Start()
+        {
             SetProgressBarValue(_playthrough.Progress);
+            _heroImage.sprite = _heroSkins.Current.Head;
+        }
 
         [Inject]
-        private void Construct(IPlaythroughProgressHandler playthrough) =>        
+        private void Construct(IPlaythroughProgressHandler playthrough, HeroSkins heroSkins)
+        {
             _playthrough = playthrough;
+            _heroSkins = heroSkins;
+        }
 
         private void SetProgressBarValue(float delta) => 
             _progressBar.value = delta;
