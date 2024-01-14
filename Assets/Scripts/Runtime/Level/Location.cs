22e235f4ae54429e384f1341f947616fd7a9ac45
Grisha Revenko <grisha@revenko.org>
2024-01-14 14:46:55 +0400

    👌 IMPROVE: Add location fields input validation & fixes

--- Assets/Scripts/Runtime/Level/Location.cs
+++ Assets/Scripts/Runtime/Level/Location.cs
@@ -1,3 +1,4 @@
+using TriInspector;
 using UnityEngine;
 
 namespace Core.Level
@@ -5,14 +6,20 @@ namespace Core.Level
     [CreateAssetMenu(fileName = "New Location", menuName = "Presets/Location")]
     public class Location : ScriptableObject
     {
+        [Required(FixAction = nameof(FixName), FixActionName = "Assign object name"), 
+        ValidateInput(nameof(ValidateName))]
         [SerializeField] private string _name;
-        [SerializeField] private BackgroundPreset _backgroundPreset;
+
+        [SerializeField, Required] private BackgroundPreset _backgroundPreset;
 
         [Space]
-        [SerializeField] private Platform _startPlatform;
+        [SerializeField, Required] private Platform _startPlatform;
 
         [Space]
+        [InfoBox("Empty - add at least one!", TriMessageType.Error, nameof(IsEnoughSimplePlatforms))]
         [SerializeField] private SimplePlatform[] _simplePlatforms;
+        [InfoBox("Empty - add few platforms!", TriMessageType.Error, nameof(IsEmptySpecialPlatforms))]
+        [InfoBox("Very few platforms", TriMessageType.Warning, nameof(IsEnoughSpecialPlatforms))]
         [SerializeField] private SpecialPlatform[] _specialPlatforms;
 
         public string Name => _name;
@@ -22,5 +29,20 @@ namespace Core.Level
         
         public SimplePlatform[] SimplePlatforms => _simplePlatforms;
         public SpecialPlatform[] SpecialPlatforms => _specialPlatforms;
+
+        private bool IsEnoughSimplePlatforms => _simplePlatforms.Length <= 0;
+        private bool IsEmptySpecialPlatforms => _specialPlatforms.Length <= 0;
+        private bool IsEnoughSpecialPlatforms => _specialPlatforms.Length == 1;
+
+        private void FixName() =>
+            _name = name;
+        
+        private TriValidationResult ValidateName()
+        {
+            if (_name.Equals(name) == false && string.IsNullOrEmpty(_name) == false)
+                return TriValidationResult.Warning("Possible name mismatch");
+            else
+                return TriValidationResult.Valid;
+        }
     }
 }
