0fd1669a9f9a7df4f465ef80a7295caf54bca189
Grisha Revenko <grisha@revenko.org>
2024-01-27 15:54:09 +0400

    👌 IMPROVE: Refactor

--- Assets/Scripts/Runtime/Level/Entities/EntitySpawner.cs
+++ Assets/Scripts/Runtime/Level/Entities/EntitySpawner.cs
@@ -44,8 +44,8 @@ namespace Core.Level
             SpawnMarker[] filtered = SpawnMarker.FilterByKind(platform.SpawnMarkers,
                 EntityKind.Chest);
 
-            SpawnSimpleChests(platform, filtered);
-            SpawnTreasureChests(platform, filtered);
+            SpawnChests(platform, filtered, ChestKind.Simple);
+            SpawnChests(platform, filtered, ChestKind.Treasure);
         }
 
         private void SpawnEnemies(Platform platform)
@@ -53,9 +53,9 @@ namespace Core.Level
             SpawnMarker[] filtered = SpawnMarker.FilterByKind(platform.SpawnMarkers,
                 EntityKind.Enemy);
                 
-            SpawnCactopuses(platform, filtered);
-            SpawnStoneys(platform, filtered);
-            SpawnCactoculuses(platform, filtered);
+            SpawnEnemies(platform, filtered, EnemyKind.Cactopus);
+            SpawnEnemies(platform, filtered, EnemyKind.Stoney);
+            SpawnEnemies(platform, filtered, EnemyKind.Cactoculus);
         }
 
         private void SetupProduct(Entity product, SpawnMarker marker)
@@ -65,73 +65,31 @@ namespace Core.Level
             marker.SetProduct(product.gameObject);
         }
 
-        private void SpawnSimpleChests(Platform platform, SpawnMarker[] filtered)
+        private void SpawnChests(Platform platform, SpawnMarker[] filtered, ChestKind kind)
         {
-            SpawnMarker[] simpleChests = filtered
+            SpawnMarker[] markers = filtered
                 .Where(marker => marker.ChestKind == ChestKind.Simple)
                 .ToArray();
 
-            Chest simpleChestPrefab = _entityAssets.Chests[ChestKind.Simple];
-            foreach (SpawnMarker marker in simpleChests)
+            Chest prefab = _entityAssets.Chests[kind];
+            foreach (SpawnMarker marker in markers)
             {
-                Chest chest = NightPool.Spawn(simpleChestPrefab, platform.transform);
+                Chest chest = NightPool.Spawn(prefab, platform.transform);
                 SetupProduct(chest, marker);
             }
         }
 
-        private void SpawnTreasureChests(Platform platform, SpawnMarker[] filtered)
+        private void SpawnEnemies(Platform platform, SpawnMarker[] filtered, EnemyKind kind)
         {
-            SpawnMarker[] treasureChests = filtered
-                .Where(marker => marker.ChestKind == ChestKind.Treasure)
+            SpawnMarker[] markers = filtered
+                .Where(marker => marker.EnemyKind == kind)
                 .ToArray();
 
-            Chest treasureChestPrefab = _entityAssets.Chests[ChestKind.Treasure];
-            foreach (SpawnMarker marker in treasureChests)
+            Enemy prefab = _enemyAssets.Enemies[kind];
+            foreach (SpawnMarker marker in markers)
             {
-                Chest chest = NightPool.Spawn(treasureChestPrefab, platform.transform);
-                SetupProduct(chest, marker);
-            }
-        }
-
-        private void SpawnCactopuses(Platform platform, SpawnMarker[] filtered)
-        {
-            SpawnMarker[] cactopusMarkers = filtered
-                .Where(marker => marker.EnemyKind == EnemyKind.Cactopus)
-                .ToArray();
-
-            Enemy cactopusPrefab = _enemyAssets.Enemies[EnemyKind.Cactopus];
-            foreach (SpawnMarker marker in cactopusMarkers)
-            {
-                Enemy enemy = NightPool.Spawn(cactopusPrefab, platform.transform);
-                SetupProduct(enemy, marker);
-            }
-        }
-
-        private void SpawnStoneys(Platform platform, SpawnMarker[] filtered)
-        {
-            SpawnMarker[] stoneyMarkers = filtered
-                .Where(marker => marker.EnemyKind == EnemyKind.Stoney)
-                .ToArray();
-
-            Enemy stoneyPrefab = _enemyAssets.Enemies[EnemyKind.Stoney];
-            foreach (SpawnMarker marker in stoneyMarkers)
-            {
-                Enemy enemy = NightPool.Spawn(stoneyPrefab, platform.transform);
-                SetupProduct(enemy, marker);
-            }
-        }
-
-        private void SpawnCactoculuses(Platform platform, SpawnMarker[] filtered)
-        {
-            SpawnMarker[] cactoculusMarkers = filtered
-                .Where(marker => marker.EnemyKind == EnemyKind.Cactoculus)
-                .ToArray();
-
-            Enemy cactoculusPrefab = _enemyAssets.Enemies[EnemyKind.Cactoculus];
-            foreach (SpawnMarker marker in cactoculusMarkers)
-            {
-                Enemy enemy = NightPool.Spawn(cactoculusPrefab, platform.transform);
-                enemy.SetLocalScale(cactoculusPrefab.GetLocalScale());
+                Enemy enemy = NightPool.Spawn(prefab, platform.transform);
+                enemy.SetLocalScale(prefab.GetLocalScale());
                 SetupProduct(enemy, marker);
             }
         }
