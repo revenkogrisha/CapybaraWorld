fe1f6aacaf90a6541caa011c4e5fccfdb037f3d5
Grisha Revenko <grisha@revenko.org>
2024-02-01 11:38:25 +0400

    📦 NEW: Relink to another cloud project (Change to UnityAds)

--- Assets/Scripts/Runtime/Mediation/UnityAdsData.cs
+++ Assets/Scripts/Runtime/Mediation/UnityAdsData.cs
@@ -2,8 +2,8 @@ namespace Core.Mediation.UnityAds
 {
     public static class UnityAdsData
     {
-        public const string AndroidGameId = "5540929";
-        public const string IOSGameId = "5540928";
+        public const string AndroidGameId = "5543641";
+        public const string IOSGameId = "5543640";
 
         public const string AndroidInterstitialId = "Interstitial_Android";
         public const string IOSInterstitialId = "Interstitial_iOS";
