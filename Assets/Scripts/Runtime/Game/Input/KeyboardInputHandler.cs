2431dd74089ccd0f347610b041fc43ca66f1de46
Grisha Revenko <grisha@revenko.org>
2024-02-20 10:52:11 +0400

    📦 NEW: Add descend option to input handler

--- Assets/Scripts/Runtime/Game/Input/KeyboardInputHandler.cs
+++ Assets/Scripts/Runtime/Game/Input/KeyboardInputHandler.cs
@@ -15,6 +15,8 @@ namespace Core.Game.Input
             _collection.MovementAction.action.performed += OnMovementStarted;
             _collection.MovementAction.action.canceled += OnMovementEnded;
 
+            _collection.DescendAction.action.performed += OnDescend;
+
             _collection.DashAction.action.performed += OnDash;
             _collection.JumpAction.action.performed += OnJump;
             _collection.JumpAction.action.canceled += OnJumpEnded;
@@ -24,6 +26,8 @@ namespace Core.Game.Input
         {
             _collection.MovementAction.action.performed -= OnMovementStarted;
             _collection.MovementAction.action.canceled -= OnMovementEnded;
+
+            _collection.DescendAction.action.performed -= OnDescend;
 
             _collection.DashAction.action.performed -= OnDash;
             _collection.JumpAction.action.performed -= OnJump;
@@ -42,11 +46,11 @@ namespace Core.Game.Input
         private void OnMovementEnded(InputAction.CallbackContext context) => 
             StopCommand.Execute();
 
-        private void OnDash(InputAction.CallbackContext context)
-        {
-            RDebug.Log(4);
+        private void OnDescend(InputAction.CallbackContext context) => 
+            DownCommand.Execute();
+
+        private void OnDash(InputAction.CallbackContext context) => 
             DashCommand.Execute();
-        }
 
         private void OnJump(InputAction.CallbackContext context)
         {
